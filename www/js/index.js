f97662e11781fe69f87a743e9a92fb9dbe80149a
Jeremy Pippin <jgpippin@gmail.com>
2013-11-14 18:03:46 -0500

    Add a prompt

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ prompt("Eat a dick");
 var app = {
     // Application Constructor
     initialize: function() {
